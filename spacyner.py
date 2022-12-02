f61b731317e56111abd6486123a76b4a6964d6e0
rfechner <richard.fechner.nr@gmail.com>
2022-12-02 11:31:30 +0100

    Added custom typesystem

--- spacyner.py
+++ spacyner.py
@@ -5,30 +5,38 @@ import spacy
 import os
 from datetime import datetime
 
+# we load a language model from spacy. Here, we use the small german model.
 nlp = spacy.load("de_core_news_sm")
 path = os.getcwd()+"/"
 
-c = ""
+# the XMI corresponding to the CAS of the prior pipeline component is in the system input stream.
+# we read every line from the input and store the XMI in string representation in the variable 'c'
+xmi_representation = ""
 for line in sys.stdin:
-    c = c + line
+    xmi_representation += line
 
-# Default Typesystem
-typesystem=load_dkpro_core_typesystem()
-# make cas out of stdin
-cas = load_cas_from_xmi(c, typesystem=load_dkpro_core_typesystem())
+# We instantiate the CAS from the typesystem and the XMI.
+with open('sample_typesystem.xml', 'rb') as f:
+    sample_typesystem = load_typesystem(f)
 
-# Get Token type
-Token = typesystem.get_type("de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity")
-# Create Spacy doc
+merged_typesystem = merge_typesystems(sample_typesystem, load_dkpro_core_typesystem())
+cas = load_cas_from_xmi(xmi_representation, typesystem=merged_typesystem)
+
+# We fetch the type from given typesystem
+Token = merged_typesystem.get_type("de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity")
+
+# create a spaCy Document instance from applying the Language model nlp to the sofa text
 doc = nlp(cas.sofa_string)
 
-# Add NE
+# Translate the spacy tokens to the dkpro cassis Token instances. Accumulate tokens into a list
 tokens = []
 # xmiID=None, value=None, identifier=None, begin=0, end=4, type='bla'
 for ent in doc.ents:
     tokens.append (Token(begin=ent.start_char, end=ent.end_char, value=ent.label_))
 
+# Add newly constructed tokens to the Common analysis system
 for t in tokens:
     cas.add(t)
-    
+
+# pass the XMI to the next pipeline component via stdout.
 print (cas.to_xmi())
